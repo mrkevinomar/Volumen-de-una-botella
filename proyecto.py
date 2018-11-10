05fbbbc4b3c6fe7db1e12165a4f76f67478baa0d
Kevin Palacios <kopalaci@espol.edu.ec>
2018-11-10 18:22:03 -0500

    calculo de distancia al enfoque

--- proyecto.py
+++ proyecto.py
@@ -7,25 +7,45 @@ def escribirTexto(imagen , texto , x ,y):
     cv2.putText(imagen, texto,(x,y), font, 0.5,(0,0,255),1,cv2.LINE_AA)
 
 
-def find_marker(image):
+def find_marker(cnts):
     # convert the image to grayscale, blur it, and detect edges
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    '''
+    gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 35, 125)
+    '''
+    # find the contours in the edged image and keep the largest one;
+    # we'll assume that this is our piece of paper in the image
+    #(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    if(len(cnts)>0):
+        c = max(cnts, key=cv2.contourArea)
+        # compute the bounding box of the of the paper region and return it
+        return cv2.minAreaRect(c)
+    return None
+
+
+def focal(imagen):
+    # convert the image to grayscale, blur it, and detect edges
+
+    gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 35, 125)
 
     # find the contours in the edged image and keep the largest one;
     # we'll assume that this is our piece of paper in the image
     (_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    c = max(cnts, key=cv2.contourArea)
 
-    # compute the bounding box of the of the paper region and return it
-    return cv2.minAreaRect(c)
-
+    if(len(cnts)>0):
+        c = max(cnts, key=cv2.contourArea)
+        # compute the bounding box of the of the paper region and return it
+        return cv2.minAreaRect(c)
+    return None
 
 def distance_to_camera(knownWidth, focalLength, perWidth):
     # compute and return the distance from the maker to the camera
     distance = (knownWidth * focalLength) / perWidth
-    print(knownWidth, focalLength, perWidth, distance)
+    #print(knownWidth, focalLength, perWidth, distance)
     return distance
 
 # initialize the known distance from the camera to the object, which
@@ -34,29 +54,26 @@ KNOWN_DISTANCE = 64
 
 # initialize the known object width, which in this case, the piece of
 # paper is 12 inches wide
-KNOWN_WIDTH = 32.0
+KNOWN_WIDTH = 7.0
 
 # initialize the list of images that we'll be using
-IMAGE_PATHS = ["Imagenes/106 2.png"]
+IMAGE_PATHS = ["Imagenes/60cm(1).png"]
 
 # load the furst image that contains an object that is KNOWN TO BE 2 feet
 # from our camera, then find the paper marker in the image, and initialize
 # the focal length
 image = cv2.imread(IMAGE_PATHS[0])
-marker = find_marker(image)
+marker = focal(image)
 print(marker)
-focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+if marker:
+    focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 #Empeiza captura
 
-
 captura = cv2.VideoCapture(0)
 while(True):
-    ret , frame_original = captura.read()
 
-    marker = find_marker(frame_original)
-    inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
-    print("Distancia" + inches)
+    ret , frame_original = captura.read()
 
     # Cambiar imagen original a gris
     img_gray = cv2.cvtColor(frame_original, cv2.COLOR_BGR2GRAY)
@@ -92,7 +109,23 @@ while(True):
         #obtenemos el roi en base a el contorno obtenido
         liquido=transformacion[yo:yo+alto,xo:xo+ancho]
         pixeles_blancos = np.sum(transformacion == 255)
-        cv2.imshow("ROI",liquido)
+#        cv2.imshow("ROI",liquido)
+
+        if(len(contornoMayor)>0):
+            #c = max(contornoMayor, key=cv2.contourArea)
+            # compute the bounding box of the of the paper region and return it
+            marker = cv2.minAreaRect(contornoMayor)
+
+            inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+            # draw a bounding box around the image and display it
+            #box = np.int0(cv2.boxPoints(marker))
+            #cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+            #cv2.putText(image, str(inches), (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+            #cv2.imshow("image", image)
+            #cv2.waitKey(0)
+
+            print("Distancia" + str(inches))
+
 
     escribirTexto(frame_original, "Pixeles Liquido :"+str(pixeles_blancos) , 20,20)
     escribirTexto(frame_original, "Volumen Liquido :"+str(volumen_liquido) +" cm3" , 20,40)
@@ -124,7 +157,7 @@ cv2.destroyAllWindows()
 #im_floodfill_inv = cv2.bitwise_not(img_binarizada)
 #
 ##Crear un kernel de '1'
-#kernel = np.ones((9,9),np.uint8)
+#kernel = np.ones((9,9),dnp.uint8)
 #
 ##Se aplica la transformacion: Closing
 #transformacion = cv2.morphologyEx(im_floodfill_inv,cv2.MORPH_CLOSE,kernel)
