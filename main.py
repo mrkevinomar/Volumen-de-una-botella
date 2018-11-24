8b43691b822c6c0f90a145e54788fd9d6fda0f72
Boscoand <boscoand12@gmail.com>
2018-11-24 13:54:30 -0500

    distancia: Refactorizacion

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ while True:
         cv2.rectangle(frame_original, (xo, yo), (xo + ancho, yo + alto), (255, 0, 0), 2)
         # Obtenemos el roi en base a el contorno obtenido
         liquido = img[yo:yo+alto, xo:xo+ancho]
-        cv2.imshow("Vide22222o", liquido)
         pixeles_blancos = np.sum(liquido == 255)
 
         # Calcular la distancia entre la botella y la cámara
